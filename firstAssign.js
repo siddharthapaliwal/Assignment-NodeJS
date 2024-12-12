207bac4e6909b809514dfd3909d4b416e773eb1b
siddharthapaliwal <155958103+siddharthapaliwal@users.noreply.github.com>
2024-12-12 14:54:04 +0530

    Add files via upload

---
+++ firstAssign.js
@@ -0,0 +1,21 @@
+/* 
+Download NodeJS
+1. Downloading the Node.js .msi installer the first step to install Node js on Windows is to download the installer.
+2. Go to Prebuilt Installer and Select the Versions
+3. Run the Node.js Installer
+    3.1 Double-click on the .msi installer to open the Node.js Setup wizard and select “Next”
+    3.2 After clicking “Next”, the End-User License Agreement (EULA) will open. Now, check “I accept the terms in the License Agreement” and select “Next”
+    3.3 Choose “Destination Folder” where you want to install Node.js & select “Next”
+4. Finish Setup and Install Node.js
+5. Verify that Node.js was properly installed or not
+    5.1 C:\Users\Admin> node -v
+
+Creating a New Application
+1. First, let's create a folder for the new application. Create a folder called "list-app" wherever you like in your PC.
+2. Running Commands and Installing Packages, We need to use a terminal/command prompt in order to run commands. You can open a terminal in VSCode by selecting on Terminal from the top menu and then clicking New Terminal.
+3. First, run the following command: npm init --yes
+4. Let's Start the Server and Display Pages.
+5. Next, let's write out some code in the files you created.
+
+*/
+console.log("Hello World");
