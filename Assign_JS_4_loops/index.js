b9532e63b7f2df2123a9d109ce83ec518dee4955
siddharthapaliwal <sid.paliwal1989@gmail.com>
2024-12-16 11:51:05 +0530

    Success

---
+++ Assign_JS_4_loops/index.js
@@ -0,0 +1,20 @@
+
+
+
+// Use a for loop to calculate the sum of numbers from 1 to num
+for (let i = 1; i <= 100; i++) {
+  
+  console.log("Value of I using for :"+i);
+}
+let j=1;
+while (j<=100)
+{
+    console.log("Value of J using while :"+j);
+    j=j+1;
+
+}
+
+// Display the result
+
+// npm install prompt-sync
+// node+space+file_Name
