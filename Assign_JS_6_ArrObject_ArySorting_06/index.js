b01a0332a2467170d346331e682ccbb82db009a4
siddharthapaliwal <sid.paliwal1989@gmail.com>
2024-12-17 12:36:02 +0530

    Success

---
+++ Assign_JS_6_ArrObject_ArySorting_06/index.js
@@ -0,0 +1,10 @@
+// Define an array of string names
+const names = ['Charlie', 'Alice', 'Bob', 'Eve', 'Dave'];
+
+console.log('Names before Sort:', names);
+// Sort the array alphabetically using the sort method
+
+const sortedNames = names.sort();
+
+// Display the sorted array
+console.log('Sorted names:', sortedNames);
