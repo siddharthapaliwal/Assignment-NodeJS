cde7a133df86d68f48a5c7d66f0617d50073e6c1
siddharthapaliwal <sid.paliwal1989@gmail.com>
2024-12-17 11:50:14 +0530

    Success

---
+++ Assign_JS_6_ArrObject_AryLen_02/index.js
@@ -0,0 +1,7 @@
+const numbers = [10, 20, 30, 40, 50, 60, 70, 80];
+
+// Calculate the length of the array
+const arrayLength = numbers.length;
+
+// Display the length of the array
+console.log(`The length of the array is: ${arrayLength}`);
