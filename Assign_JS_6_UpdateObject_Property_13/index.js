f63a9594c9d9e54587009039b4152f8cc0d64df0
siddharthapaliwal <sid.paliwal1989@gmail.com>
2024-12-17 16:17:00 +0530

    Success

---
+++ Assign_JS_6_UpdateObject_Property_13/index.js
@@ -0,0 +1,13 @@
+// Define an object representing a person
+const person = {
+    name: 'John Doe',
+    age: 25
+};
+
+
+console.log('Value before update :', person);
+
+person.age = 30;
+
+// Log the updated object to the console
+console.log('Updated Person Object:', person);
