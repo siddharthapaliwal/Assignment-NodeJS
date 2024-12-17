67c0a652bbdee909e69a5f8bb43a93dffd42b8f9
siddharthapaliwal <sid.paliwal1989@gmail.com>
2024-12-17 12:38:22 +0530

    Success

---
+++ Assign_JS_6_ArrObject_AryMerge_07/index.js
@@ -0,0 +1,13 @@
+// Define two separate arrays
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+
+console.log('First Array:', array1);
+console.log('Second Array:', array2);
+
+
+// Merge the arrays using the concat method
+const mergedArray = array1.concat(array2);
+
+// Log the merged array to the console
+console.log('Merged Array:', mergedArray);
