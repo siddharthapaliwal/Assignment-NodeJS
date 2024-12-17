66a1d1229b3a2802e1a212b4f97a01e722bee5d1
siddharthapaliwal <sid.paliwal1989@gmail.com>
2024-12-17 11:53:22 +0530

    Success

---
+++ Assign_JS_6_ArrObject_AryAddRemoveEle_03/index.js
@@ -0,0 +1,15 @@
+// Define an array with three fruits
+const fruits = ['Apple', 'Banana', 'Cherry'];
+
+// Add a new fruit to the end of the array using the push method
+console.log('Orignal array', fruits);
+
+fruits.push('Mango');
+console.log('After adding a new fruit:', fruits);
+
+// Remove the last fruit from the array using the pop method
+const removedFruit = fruits.pop();
+console.log('Removed fruit:', removedFruit);
+
+// Display the updated array
+console.log('Updated array:', fruits);
