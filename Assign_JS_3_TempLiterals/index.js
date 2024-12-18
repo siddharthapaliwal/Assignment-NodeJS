2f0a8f0cd85caebd368e301abd1ebed42aef83ad
siddharthapaliwal <sid.paliwal1989@gmail.com>
2024-12-18 11:16:17 +0530

    Success

---
+++ Assign_JS_3_TempLiterals/index.js
@@ -0,0 +1,10 @@
+// Defining variables
+const firstName = "Siddharth";
+const lastName = "Paliwal";
+const age = 34;
+
+// Using a template literal to create a greeting string
+const greeting = `Hello, my name is ${firstName} ${lastName} and I am ${age} years old.`;
+
+// Logging the greeting to the console
+console.log(greeting);
