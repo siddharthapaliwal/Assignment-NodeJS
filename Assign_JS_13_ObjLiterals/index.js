b4bb6b952cf2ff20f0f093139cdab95c2e70990f
siddharthapaliwal <sid.paliwal1989@gmail.com>
2024-12-18 12:41:44 +0530

    Success

---
+++ Assign_JS_13_ObjLiterals/index.js
@@ -0,0 +1,8 @@
+// 1. Create variables for name and age
+const name = 'Siddharth';
+const age = 34;
+
+// 2. Use enhanced object literals to create an object
+const person = { name, age };
+
+console.log(person); // Output
