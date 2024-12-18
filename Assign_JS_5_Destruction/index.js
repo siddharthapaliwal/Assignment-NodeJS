6dbfef2ec4f1091bbf4a410755898ebb4dd24310
siddharthapaliwal <sid.paliwal1989@gmail.com>
2024-12-18 12:15:30 +0530

    Sucess

---
+++ Assign_JS_5_Destruction/index.js
@@ -0,0 +1,9 @@
+// Destructuring assignment with an object
+const person = { name: "Siddharth", age: 34, city: "Bhilwara" };
+
+// Destructuring the object into variables
+const { name, age, city } = person;
+
+console.log(name); 
+console.log(age);   
+console.log(city);  
